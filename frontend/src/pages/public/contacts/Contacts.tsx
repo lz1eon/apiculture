61eebada0a8a5013aeb64b539f43fd6ce80de99b
Aleksandar Stefanov <stefanov.alexandre@gmail.com>
2024-01-09 18:15:18 +0200

    UI: improved Contacts page

--- frontend/src/pages/public/contacts/Contacts.tsx
+++ frontend/src/pages/public/contacts/Contacts.tsx
@@ -1,6 +1,6 @@
-import { IonCol, IonGrid, IonLabel, IonRow, IonText } from "@ionic/react"
+import { IonCard, IonCardContent, IonCardHeader, IonCardSubtitle, IonCardTitle, IonCol, IonGrid, IonIcon, IonImg, IonLabel, IonRow, IonText } from "@ionic/react"
 import Page from '../../Page';
-
+import { call, mail, phoneLandscape } from "ionicons/icons";
 
 export const Contacts = () => {
   return (
@@ -8,32 +8,49 @@ export const Contacts = () => {
       <IonGrid>
         <IonRow>
           <IonCol></IonCol>
-          <IonCol>
-            <IonText><h2>"Пчелинът" ("Apiarium") е разработка и собственост на Апиариум ЕООД</h2></IonText>
+          <IonCol size="6">
+            <IonCard>
+              <IonCardHeader>
+                <IonCardTitle>
+                  <h2>"Пчелинът" ("Apiarium")</h2>
+                </IonCardTitle>
+                <IonCardSubtitle>
+                  <h3>Разработка и собственост на "Апиариум" ЕООД</h3>
+                </IonCardSubtitle>
+                <IonCardSubtitle>
+                  <h3><IonText>За контакти:</IonText></h3>
+                  <h2><IonText>Александър Стефанов</IonText></h2>
+                </IonCardSubtitle>
+              </IonCardHeader>
+              <IonCardContent>
+                <IonGrid>
+                  <IonRow><IonCol></IonCol></IonRow>
+                  <IonRow>
+                    <IonCol>
+                    </IonCol>
+                  </IonRow>
+                  <IonRow>
+                    <IonCol>
+                      <IonIcon size="large" src={mail} />
+                    </IonCol>
+                    <IonCol>
+                      <IonIcon size="large" src={call} />
+                    </IonCol>
+                  </IonRow>
+                  <IonRow>
+                    <IonCol>
+                      <IonText>stefanov.alexandre@gmail.com</IonText>
+                    </IonCol>
+                    <IonCol>
+                      <IonText>(+359) 887150809</IonText>
+                    </IonCol>
+                  </IonRow>
+                  <IonRow></IonRow>
+                </IonGrid>
+              </IonCardContent>
+            </IonCard>
           </IonCol>
-          <IonCol></IonCol>                        
-        </IonRow>
-        <IonRow>
           <IonCol></IonCol>
-          <IonCol>
-            <IonText><h3>За контакти</h3></IonText>
-            <IonLabel>Александър Стефанов</IonLabel>
-          </IonCol>
-          <IonCol></IonCol>                        
-        </IonRow>
-        <IonRow>
-          <IonCol></IonCol>
-          <IonCol>
-            <IonLabel><a href="mailto:stefanov.alexandre@gmail.com">stefanov.alexandre@gmail.com</a></IonLabel>
-          </IonCol>
-          <IonCol></IonCol>                        
-        </IonRow>
-        <IonRow>
-          <IonCol></IonCol>
-          <IonCol>
-            <IonLabel>+359887150809</IonLabel>
-          </IonCol>
-          <IonCol></IonCol>                        
         </IonRow>
       </IonGrid>
     </Page>
